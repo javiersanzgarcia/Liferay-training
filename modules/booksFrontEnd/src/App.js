fbb90ff2735c003f6ef290f0edd98786726efe38
Alberto Ruiz <albertzruiz1@outlook.es>
2021-07-08 09:33:50 +0200

    requiered changes added to frontend

--- modules/booksFrontEnd/src/App.js
+++ modules/booksFrontEnd/src/App.js
@@ -15,7 +15,10 @@ import {
 } from "reactstrap";
 
 const App = () => {
+
+
   const baseUrl = "http://localhost/o/library/";
+  let permisions = [];
 
   const [books, setBooks] = useState([]);
   const [modal, setModal] = useState(false);
@@ -23,16 +26,50 @@ const App = () => {
   const [isbn, setIsbn] = useState(0);
   const [title, setTitle] = useState("");
   const [writer, setWriter] = useState("");
+  const [date, setDate] = useState("");
+  const [editMode,setEditMode]  = useState(false);
 
-  const toggle = () => setModal(!modal);
+  const toggle = () => {
+    setEditMode(false)
+    setIsbn(0);
+    setTitle("");
+    setWriter("");
+    setDate("");
+    setModal(!modal);
+  }
+
+  const openEdit = (book) => {
+    setEditMode(true)
+    setIsbn(book.isbn);
+    setTitle(book.title);
+    setWriter(book.writer);
+    setDate(new Date(book.publication).toISOString().substr(0,10));
+    setModal(true);
+  }
 
   useEffect(() => {
     retrieveBooks();
   }, []);
 
+
+  const findBooks = (search) => {
+    axios.get(baseUrl + "find",{params: {query: search}}).then((response) => {
+      setBooks(response.data);
+    });
+
+  }
+
   const retrieveBooks = () => {
-    axios.get(baseUrl + "getAll").then((response) => setBooks(response.data));
+    axios.get(baseUrl + "getAll").then((response) => {
+      setBooks(response.data);
+    });
   };
+
+  const retrieveSingleBook = (isbn) => {
+    axios.get(baseUrl + "getOne",{params: {isbn: isbn}}).then((response) => {
+      return response.data;
+    });
+  }
 
   const saveBooks = (book) => {
     axios.post(baseUrl, book).then((response) => {
@@ -42,6 +79,31 @@ const App = () => {
     });
   };
 
+  const deleteBook = (book) => {
+    axios.delete(baseUrl, book).then((response) => {
+      if (response.status === 200) {
+        retrieveBooks();
+      }
+    });
+  }
+
+  const editBook = () => {
+    
+    const newBook = {
+      isbn: parseInt(isbn),
+      title,
+      writer,
+      publication: date,
+    };
+    axios.put(baseUrl + "update",{
+      newBook
+    }).then(response => {
+      if (response.status === 200) {
+        retrieveBooks();
+      }
+    })
+  }
+
   const addBook = (event) => {
     // event.preventDefault();
 
@@ -49,15 +111,72 @@ const App = () => {
       isbn: parseInt(isbn),
       title,
       writer,
-      publication: null,
+      publication: date,
     };
 
     saveBooks(bookAdded);
     toggle();
   };
 
+
+
+  const guestContent = (books) => {
+
+    return(
+      <tbody>
+        {books &&
+        books.map((book) => (
+          <tr key={book.isbn}>
+            <td>{book.isbn}</td>
+            <td>{book.title}</td>
+            <td>{book.writer}</td>
+            <td>{new Date(book.publication).toLocaleDateString()}</td>
+            <td>
+              
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+
+  const adminContent = (books) => {
+
+    return(
+      <tbody>
+        {books &&
+        books.map((book) => (
+          <tr key={book.isbn}>
+            <td>{book.isbn}</td>
+            <td>{book.title}</td>
+            <td>{book.writer}</td>
+            <td>{new Date(book.publication).toLocaleDateString()}</td>
+            <td>
+              {permisions.indexOf("edit-book") != -1 && <Button active={} color="secundary" onClick={(event) => openEdit(book)}>
+                Edit
+              </Button>}
+              {permisions.indexOf("delete-book") != -1 && <Button color="danger" onClick={(event) => deleteBook(book)}>
+                Delete
+              </Button>}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+
   return (
     <div>
+      <form class="form-search">
+       <Input
+          class="input-medium search-query"
+          placeholder="Search"
+          type="search"
+          name="writer"
+          onChange={(event) => findBooks(event.target.value)}
+            />
+
+      </form>
       <h4>Book List</h4>
       <Table striped responsive>
         <thead>
@@ -66,39 +185,39 @@ const App = () => {
             <th>Title</th>
             <th>Writer</th>
             <th>Publication</th>
+            <th>Options</th>
           </tr>
-        </thead>
-        <tbody>
-          {books &&
-            books.map((book) => (
-              <tr key={book.isbn}>
-                <td>{book.isbn}</td>
-                <td>{book.title}</td>
-                <td>{book.writer}</td>
-                <td>{book.publication}</td>
-              </tr>
-            ))}
-        </tbody>
+        </thead>  
+        {(Liferay.ThemeDisplay.getUserName() === "")
+         ? guestContent(books)
+         : adminContent(books)  }
+        
       </Table>
-      <Button color="success" onClick={toggle}>
-        Add Book
-      </Button>
+      {Liferay.ThemeDisplay.isSignedIn() && 
+        <Button color="success" onClick={toggle}>
+          Add Book
+        </Button>
+      }
+
       <Modal isOpen={modal} toggle={toggle}>
         <form>
-          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={toggle}>{editMode ? "Save Book" : "Add Book"}</ModalHeader>
           <ModalBody>
             <Label for="iDisbn">ISBN</Label>
             <Input
+              disabled={editMode}
               type="number"
               name="isbn"
               id="iDisbn"
-              onChange={(event) => setIsbn(event.target.value)}
+              value={isbn}
+              onChange={(event) =>  setIsbn(event.target.value)}
             />
             <Label for="iDtitle">Title</Label>
             <Input
               type="text"
               name="title"
               id="iDtitle"
+              value={title}
               onChange={(event) => setTitle(event.target.value)}
             />
             <Label for="iDwriter">Writer</Label>
@@ -106,14 +225,24 @@ const App = () => {
               type="text"
               name="writer"
               id="iDwriter"
+              value={writer}
               onChange={(event) => setWriter(event.target.value)}
+            />
+            <Label for="iDdate">Date</Label>
+            <Input
+              type="date"
+              name="date"
+              id="iDdate"
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
             />
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={(event) => addBook(event)}>
-              Add
-            </Button>
-            <Button color="danger" onClick={toggle}>
+
+            {editMode
+            ? <Button color="primary" onClick={(event) =>  editBook(event)}>Save</Button>
+            : <Button color="primary" onClick={(event) =>  addBook(event)}>Add</Button>}
+            <Button color="danger" onClick={toggle}> 
               Cancel
             </Button>
           </ModalFooter>
